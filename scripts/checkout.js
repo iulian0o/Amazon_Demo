f2ecc1f33fc4e27cfaef08ea9b303d2ec5a622af
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-06-19 18:59:39 +0200

    refactor: Calculate basket quantity refactored in one function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -105,5 +105,16 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
       `.js-cart-item-conatiner-${productId}`,
     );
     container.remove();
+    
+    updateCartQuantity();
   });
 });
+
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector(".js-return-to-home-link").innerHTML =
+    `${cartQuantity} items`;
+}
+
+updateCartQuantity();
